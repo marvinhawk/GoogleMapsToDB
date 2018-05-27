b2c03bfc516abaa1f686c8a8458c80edb36baef7
marvinhawk <martinhauge@gmail.com>
2018-05-27 16:34:38 +0200

    mapsdb.py
    Version 1.2.

--- mapsdb.py
+++ mapsdb.py
@@ -1,62 +1,92 @@
 #
-# 1.0. Google Maps to Database
-# Input: sanitised KML file saved as .xml
-# Output: CSV file with or without headers
+# 1.2. Google Maps to Database
+# Input: KML file
+# Output: Name and coordinates of locations in CSV file with or without headers
 #
 
-from xml.etree import ElementTree as ET
-import csv
+import re, csv
 
-class Location:
-	name = ""
-	latitude = 0
-	longitude = 0
+def openFile():
+	print("Input KML file")
+	while(True):
+		filename = input()
+		if filename.lower().endswith('.kml'):
+			try:
+				infile = open(filename)
+				return (infile, filename)
+			except IOError:
+				print("Couldn't open {}".format(filename))
+		else:
+			print("Not a KML file")
 
-locations = []
-
-headers = 0
-
-print("Input XML file")
-while(True):
-	filename = input()
-	if filename.endswith('.xml'):
-		try:
-			infile = open(filename)
-			break
-		except IOError:
-			print("Couldn't open {}".format(filename))
-	else:
-		print("Not an XML file")
-
-outfile = filename.split('.')[0] + '-output.csv'
-
-tree = ET.parse(infile)
-root = tree.getroot()
-
-for placemark in root.iter('Placemark'):
-	location = Location()
-	location.name = placemark[0].text
+def searchFile(text):
+	class Location:
+		name = ""
+		latitude = 0
+		longitude = 0
 	
-	rawcoordinates = placemark[2][0].text
-	cleancoordinates = rawcoordinates.split()
-	latLon = cleancoordinates[0].split(',')
-	location.latitude = latLon[0]
-	location.longitude = latLon[1]
+	locations = []
 
-	locations.append(location)
+	search = re.compile(r'''
+		<Placemark>
+		.*?
+		<name>
+		(.*?)
+		</name>
+		.*?
+		<coordinates>
+		(.*?)
+		</coordinates>
+		.*?
+		</Placemark>
+		''', re.DOTALL | re.VERBOSE)
+	
+	results = search.findall(text)
 
-f = open(outfile, 'w')
+	for result in results:
+		location = Location()
 
-writer = csv.writer(f)
+		if result[0].startswith('<![CDATA['):
+			name = result[0][len('<![CDATA['):].rstrip(']]>')
+		else:
+			name = result[0]
 
-while(headers != 'yes' and headers != 'y' and headers != 'no' and headers != 'n'):
-	print('Include headers?')
-	headers = input()
-	if headers == 'yes' or headers == 'y':
-		writer.writerow(('Name', 'Latitude', 'Longitude'))
+		location.name = name
 
-for location in locations:
-	writer.writerow((location.name, location.latitude, location.longitude))
+		coordinates = result[1].strip().split(',')
 
-f.close()
-print("CSV file created")
+		location.latitude = coordinates[0]
+		location.longitude = coordinates[1]
+		
+		locations.append(location)
+		
+	return locations
+
+
+def printFile(loc, name):
+	outfile = name.split('.')[0] + '-output.csv'
+	headers = 0
+
+	f = open(outfile, 'w')
+
+	writer = csv.writer(f)
+
+	while(headers != 'yes' and headers != 'y' and headers != 'no' and headers != 'n'):
+		print('Include headers?')
+		headers = input()
+		if headers == 'yes' or headers == 'y':
+			writer.writerow(('Name', 'Latitude', 'Longitude'))
+
+	for location in loc:
+		writer.writerow((location.name, location.latitude, location.longitude))
+
+	f.close()
+	print("CSV file created")
+
+infile, filename = openFile()
+
+fileStr = infile.read()
+
+places = searchFile(fileStr)
+
+printFile(places, filename)
